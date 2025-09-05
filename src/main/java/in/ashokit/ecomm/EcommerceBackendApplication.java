076542dba2f4c02805f4c5b401628cdb80b1b65d
shekharpanchal <120018535+shekharpanchal@users.noreply.github.com>
2025-09-06 02:33:54 +0530

    Create EcommerceBackendApplication.java

---
+++ src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
@@ -0,0 +1,15 @@
+package in.ashokit.ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendApplication.class, args);
+		int j = 10;
+		
+	}
+
+}
